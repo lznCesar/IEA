7ce99418e7955a0ff1d1efe35d49f98e29615fb4
lznCesar <celori54@gmail.com>
2020-08-21 09:55:20 -0500

    version de codigo 4 21/08/20

--- Js/app.js
+++ Js/app.js
@@ -1,25 +1,48 @@
+/*Codigo creado por Cesar Lozano Rincon*/
+
 // declaración de variables
 let nombre, apellidoPaterno, apellidoMaterno, curp;
 
 // Referencias al Html
-const btnGuardar = document.querySelector('#guardar');
- 
+const btnGuardar = document.querySelector("#guardar"),
+      btnGenerarXml = document.querySelector("#generar");
+      btnGenerarXml.disabled = true;
+     
 
 /* función para guardar la información
  que se agregaron al formulario */
-btnGuardar.addEventListener('click',()=>{
+btnGuardar.addEventListener("click", () => {
+    nombre          = document.querySelector("#nombre").value;
+    apellidoPaterno = document.querySelector("#primerApellido").value;
+    apellidoMaterno = document.querySelector("#segundoApellido").value;
+    curp            = document.querySelector("#curp").value;
 
-    nombre          = document.querySelector('#nombre').value;
-    apellidoPaterno = document.querySelector('#primerApellido').value;
-    apellidoMaterno = document.querySelector('#segundoApellido').value;
-    curp            = Number(document.querySelector('#curp').value);
+    console.log(nombre, apellidoPaterno, apellidoMaterno, curp);
 
-    console.log(nombre,apellidoPaterno,apellidoMaterno,curp);
-    
-    return nombre, apellidoPaterno, apellidoMaterno, curp
 
-})
+  // validaciones de los formularios
+   if(nombre === "" || apellidoPaterno === '' 
+   || apellidoMaterno === '' || curp === ''){
+        alert(
+            'los campos no  estan completos, identifique su contenido')
+   }else{
+        alert('se ha llenado los campos satisfactoriamente');
+        btnGuardar.disabled = true;
+        btnGenerarXml.style.backgroundColor = 'green'
+        btnGenerarXml.disabled = false;
 
+   }   
+      return nombre, apellidoPaterno, apellidoMaterno, curp;
+});
+
+
+
+//funcion para generar archivo Xml
+/* 
+btnXml.addEventListener('click', crearDocXml())
+function crearDocXml(){
+console.log('entro a funcion crear xml');
+}  */
 
 
 
